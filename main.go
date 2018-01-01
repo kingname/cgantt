0295ae794038ca7be8bbe0142d4ef834368ce92d
kingname <greensouth@foxmail.com>
2018-01-01 21:14:56 +0800

    runable

--- main.go
+++ main.go
@@ -6,24 +6,26 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+
+	"./task"
 )
 
 func main() {
 	width := getCurrentTerminalWidth()
-	line1 := Line{"-", width / 2, "", 0, "=", width / 2}
-	line2 := Line{"-", width / 3, "", 0, "=", 2 * width / 3}
-	line3 := Line{"-", width / 4, "", 0, "=", 3 * width / 4}
-	line4 := Line{"-", width / 5, "", 0, "=", 4 * width / 5}
-	line5 := Line{"-", 5 * width / 6, "", 0, "=", width / 6}
-	line6 := Line{"-", 3 * width / 7, "", 0, "=", 4 * width / 7}
-	line7 := Line{"-", 4 * width / 8, "", 0, "=", width / 2}
-	DrawLine(line1)
-	DrawLine(line2)
-	DrawLine(line3)
-	DrawLine(line4)
-	DrawLine(line5)
-	DrawLine(line6)
-	DrawLine(line7)
+	line1 := task.Line{"-", width / 2, "", 0, "=", width / 2}
+	line2 := task.Line{"-", width / 3, "", 0, "=", 2 * width / 3}
+	line3 := task.Line{"-", width / 4, "", 0, "=", 3 * width / 4}
+	line4 := task.Line{"-", width / 5, "", 0, "=", 4 * width / 5}
+	line5 := task.Line{"-", 5 * width / 6, "", 0, "=", width / 6}
+	line6 := task.Line{"-", 3 * width / 7, "", 0, "=", 4 * width / 7}
+	line7 := task.Line{"-", 4 * width / 8, "", 0, "=", width / 2}
+	task.DrawLine(line1)
+	task.DrawLine(line2)
+	task.DrawLine(line3)
+	task.DrawLine(line4)
+	task.DrawLine(line5)
+	task.DrawLine(line6)
+	task.DrawLine(line7)
 }
 
 func raiseErr(err error) {
